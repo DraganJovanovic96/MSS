e09c054ae98e7ec8c21c4b2c4f70bf981dcf785c
DESKTOP-I0S9ABL\davel <dragan.jovanovic.gale@gmail.com>
2024-11-13 21:30:59 +0000

    mss-58: Expose and accept refresh header, optimize permanent deletion

--- src/main/java/com/mss/service/impl/PermanentlyDeleteServiceImpl.java
+++ src/main/java/com/mss/service/impl/PermanentlyDeleteServiceImpl.java
@@ -1,9 +1,6 @@
 package com.mss.service.impl;
 
-import com.mss.model.Customer;
-import com.mss.model.Service;
-import com.mss.model.ServiceType;
-import com.mss.model.Vehicle;
+import com.mss.model.*;
 import com.mss.repository.*;
 import com.mss.service.PermanentDeletionService;
 import jakarta.transaction.Transactional;
@@ -32,6 +29,11 @@ public class PermanentlyDeleteServiceImpl implements PermanentDeletionService {
      * The repository used to retrieve vehicle data.
      */
     private final VehicleRepository vehicleRepository;
+
+    /**
+     * The repository used to retrieve token data.
+     */
+    private final TokenRepository tokenRepository;
 
     /**
      * The repository used to retrieve user data.
@@ -65,24 +67,23 @@ public class PermanentlyDeleteServiceImpl implements PermanentDeletionService {
     @Override
     @Transactional
     @Scheduled(cron = "0 59 23 ? * SUN", zone = "GMT+2")
-//    @Scheduled(cron = "0 0/1 * 1/1 * *", zone = "GMT+2") --> This is just for testing
-//    and will be deleted later on.
+//    @Scheduled(cron = "0 0/1 * 1/1 * *", zone = "GMT+2")  this is just for testing, it
+//    will be deleted later
     public void permanentlyDeleteResources() {
         List<Customer> deletedCustomers = customerRepository.findAllDeletedCustomers();
         List<Vehicle> deletedVehicles = vehicleRepository.findAllDeletedVehicles();
         List<Service> deletedServices = serviceRepository.findAllDeletedServices();
         List<ServiceType> deletedServiceTypes = serviceTypeRepository.findAllDeletedServiceTypes();
+        List<Token> tokens = tokenRepository.findAll();
 
-        deletedServiceTypes.forEach(serviceType ->
-                serviceTypeRepository.permanentlyDeleteServiceTypeById(serviceType.getId()));
+        serviceTypeRepository.deleteAll(deletedServiceTypes);
 
-        deletedServices.forEach(service ->
-                serviceRepository.permanentlyDeleteServiceById(service.getId()));
+        serviceRepository.deleteAll(deletedServices);
 
-        deletedVehicles.forEach(vehicle ->
-                vehicleRepository.permanentlyDeleteVehicleById(vehicle.getId()));
+        vehicleRepository.deleteAll(deletedVehicles);
 
-        deletedCustomers.forEach(customer ->
-                customerRepository.permanentlyDeleteCustomerById(customer.getId()));
+        customerRepository.deleteAll(deletedCustomers);
+
+        tokenRepository.deleteAll(tokens);
     }
 }
